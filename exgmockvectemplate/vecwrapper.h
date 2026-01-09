ec2854bf505753104137753ae1e48c84e0bf5b0c
mikeg64 <mikeg2105@gmail.com>
2026-01-09 19:45:57 +0000

    additional methods for vector

--- exgmockvectemplate/vecwrapper.h
+++ exgmockvectemplate/vecwrapper.h
@@ -20,6 +20,28 @@ public:
         return data_.size();
     }
 
+
+
+    // Resize the vector
+    void resize(std::size_t n) {
+        data_.resize(n);
+    }
+
+    // Assign a single value to all elements
+    void assign_one(const T& value) {
+        for (auto& elem : data_) {
+            elem = value;
+        }
+    }
+
+// Assign a value to a specific element
+void set(std::size_t index, const T& value) {
+    if (index >= data_.size()) {
+        throw std::out_of_range("VecWrapper::set index out of range");
+    }
+    data_[index] = value;
+}
+
     const T& operator[](std::size_t i) const {
         return data_[i];
     }
